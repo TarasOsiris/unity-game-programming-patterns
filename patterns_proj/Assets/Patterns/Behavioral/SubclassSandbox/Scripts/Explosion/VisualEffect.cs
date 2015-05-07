85e901a42cdfbad6bfd283a7507de94e439f2972
Taras Leskiv <leskiv.taras@gmail.com>
2015-05-07 17:24:12 +0200

    subclass sandbox changes

--- patterns_proj/Assets/Patterns/Behavioral/SubclassSandbox/Scripts/Explosion/VisualEffect.cs
+++ patterns_proj/Assets/Patterns/Behavioral/SubclassSandbox/Scripts/Explosion/VisualEffect.cs
@@ -1,17 +1,14 @@
 ﻿using UnityEngine;
 
-public abstract class VisualEffect : MonoBehaviour
-{
-    public GameObject explosionPrefab;
+public abstract class VisualEffect : MonoBehaviour {
+	[SerializeField] private GameObject explosionPrefab;
 
-    protected virtual void Start()
-    {
-        SpawnParticles();
-        Debug.Log("BaseExplosion Start()");
-    }
-    
-    protected void SpawnParticles()
-    {
-        NGUITools.AddChild(gameObject, explosionPrefab);
-    }
+	protected virtual void Start() {
+		SpawnParticles();
+		Debug.Log("BaseExplosion Start()");
+	}
+
+	private void SpawnParticles() {
+		NGUITools.AddChild(gameObject, explosionPrefab);
+	}
 }
